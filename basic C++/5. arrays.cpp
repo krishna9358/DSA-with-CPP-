9535100977b52605935f251f60b20e1a82fea621
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-04 22:36:22 +0530

    added arrays file

---
+++ basic C++/5. arrays.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+// array : when want to store multiple value with same data type in consecutive memory allocation;
+// tyeps : 1d array 2d aaray
+// dataType name[];
+// name[indexArray] = value;
+
+int num[5];
+num[0] = 1;
+
+//    int num [0,1,2,3,4] = [1,2,3,4,5]; not the right way to write an array
+// index                     0 1 2 3 4
+/// operations and modifications can be done one these.
+
+
+// 2d Array
+// dataType name[rows][columns] ;
+ int number[3][5];
+ number[1][3] = 1;
+ cout << number[1][3];
+
+ // 1 2 3 4 5 
+ // 2 3 4 5 6
+ // 3 4 5 6 7
+
+
+//some garbage value can be stored to the places where you have not assigned the vlaue. 
+    return 0;
+}
