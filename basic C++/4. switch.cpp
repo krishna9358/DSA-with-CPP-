1f047f7663dd0c241b260eb8427e5b5e788dc87c
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-04 22:35:31 +0530

    added swithc statement file

---
+++ basic C++/4. switch.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+//    int a = 5;
+   int Day =3 ;
+   switch (Day){
+    case 1  : cout<< "monday"; break;
+
+    case 5 : cout << "friday";
+    default : cout << "nothing wrong input";
+   }
+    return 0;
+}
