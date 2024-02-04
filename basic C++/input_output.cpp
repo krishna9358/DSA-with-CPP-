28f9ea7b2a1107e7e6d833ab1e6d2ebce12030ba
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-04 22:32:15 +0530

    added input output file.

---
+++ basic C++/input_output.cpp
@@ -0,0 +1,17 @@
+// skelton 
+#include <iostream> //to input and out
+// #include <math.h> 
+#include <bits/stdc++.h>
+// hack -> #include <bits/stdc++.h> it includes all the library in cpp
+using namespace std; // will give std to everyname
+
+//cout -> to print 
+//cin -> to take input
+int main (){
+    int x,y;
+    std::cout << "hello world "; // "\n" or endl for new line 
+   cout << "enter any  two digit : ";
+    cin >> x >> y ; 
+
+    return 0;
+}
