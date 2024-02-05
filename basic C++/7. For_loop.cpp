f08a6e80e88210883a37e415b984b9479144f270
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-06 00:15:30 +0530

    added for loops and while, do while loops

--- basic C++/7. For_loop.cpp
+++ basic C++/7. For_loop.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 
 int main(){
+    // for (declaration ; condition to stop;  process to do){
+        //code to run;
+        // scope of i is in this only
+    // }
 
+    for (int i = 0 ; i< 10; i++){
+        cout << "Hii " << i <<endl;
+    }
+
+    // in reverse 
+    for (int j=10; j >0; j--){
+        cout << j << endl;
+    }
+
+    //nested for loop are loosp inside the loops
 
     return 0;
 }
--- basic C++/8. While_loop.cpp
+++ basic C++/8. While_loop.cpp
@@ -2,7 +2,26 @@
 using namespace std;
 
 int main(){
+    // it only runs if the condition is true, if condition is false it wont even run once. 
+    // while ( condition ) {
+        // code to run until condition is true;
+    //}
+    int i = 0;
+    while (i < 10 ) {
+        i++ ;       
+        cout <<  i << endl ;
+    }
 
 
+    // do while loop 
+    // it ateast runs one time even if the condition is not true
+    // do {
+        // code to run 
+    //} while ( codition ) ;
+
+        do{
+            cout << endl << "result for do while " << i << endl;
+        }while (i > 10);
+    
     return 0;
 }
