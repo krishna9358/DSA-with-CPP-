7f0befb2e3039624271f26ff95ecbf6731dbdf74
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-07 22:23:26 +0530

    functions completed

--- basic C++/9. Functions.cpp
+++ basic C++/9. Functions.cpp
@@ -14,6 +14,7 @@ void name_with_parameters(string name_para){
     //functions given hence parameterised function 
 }
 
+
 //return function
 int sum(int a, int b){
     // functin which return some value 
@@ -22,6 +23,21 @@ int sum(int a, int b){
     // need to store this return value in some var to use it 
     // make sure to return when use int, otherwise it will throw some garbage value.
 }
+
+//pass by value;
+int changeString(string s ){
+    cout << endl << s << endl; 
+    s[0] = 'P';
+    cout << s << endl;
+}
+
+//pass by reference;
+int changeString2(string &s2 ){
+    cout << endl << s2 << endl; 
+    s2[0] = 'P';
+    cout << s2 << endl;
+}
+
 
 int main(){
     // functions are code of blocks which perform a paticular task 
@@ -54,5 +70,22 @@ int main(){
     // 2. max(a,b)  --> tells maximum number out of 2
 
 
+// function pass by value;
+// one copy is created and it changes for that particular function, the original value does not change
+    cout << endl << "pass by value function";
+    string s = "Krishna";
+    changeString(s);
+    cout << s ; 
+
+
+// function pass by reference;
+// instead of passing the copy, you pass the reference to change in the actual/original value. You do it using & functoin with arguement.
+// array, map , list , vector all these are passed by only reference. 
+        cout << endl << "pass by value function";
+    string s2 = "Krishna";
+    changeString2(s2);
+    cout << s2 ; 
+
     return 0;
 }
+
