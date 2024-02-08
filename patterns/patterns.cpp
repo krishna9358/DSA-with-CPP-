e228a7deac5e1397dc24209ac21e30fbb4332c95
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-08 16:46:32 +0530

    added pattern 1

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -12,6 +12,14 @@ int main(){
         // 3. Print them "*" inside the inner for loop
         // 4. observe symmetry (optional)
 
+    // Pattern 1 
+    for ( int i = 0 ;i<4 ; i++){
+        for (int j = 0; j <4 ;j++){
+            cout << " * " ;
+        }
+        cout <<endl;
+    }
+
     
 
     return 0;
