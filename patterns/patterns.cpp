4e2338faede3d9b20ff8b950ffa99d3a864a7662
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-08 16:42:13 +0530

    added the theory for pattern and created file

---
+++ patterns/patterns.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // patterns
+    // mostly 2 nested loops --> outer loop, inner loop
+    // outer loop --> rows , inner loop --> columns
+
+    // Rules 
+        // 1. for the outer loop, count the no. of lines
+        // 2. for the inner loop, focus on the columns and connect them somehow to the rows
+        // 3. Print them "*" inside the inner for loop
+        // 4. observe symmetry (optional)
+
+    
+
+    return 0;
+}
