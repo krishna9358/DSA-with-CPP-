8af96c82d1a9902bc39e65dd4af00b14ffb6c037
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-10 18:53:15 +0530

    added pattern 8

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -78,14 +78,16 @@ void pattern7(int n){
         
         cout << endl;
     }
+    cout << endl;
 }
+
 
 //pattern 8
 void pattern8(int n){
     for (int i = 0; i< n; i++){
         for (int j = 0 ; j<i; j++){
             cout << " ";}
-        for (int k = 0 ; k<=n-i-1; k++){
+        for (int k = 0 ; k<2*n-(2*i+1); k++){
             cout << "*";}
         for (int j = 0 ; j<i; j++){
             cout << " ";
