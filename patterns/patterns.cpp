d5cc32e0f0648512db89a793f6a679aee3ff3b28
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-12 23:08:37 +0530

    added pattern 12

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -139,6 +139,8 @@ void pattern10(int n){
     
 }
 
+
+
 //pattern 11
 void pattern11(int n){
     int start =1 ;
@@ -151,11 +153,22 @@ void pattern11(int n){
         }
         cout << endl;
     }
+    cout << endl;
 }
 
 
 //pattern 12
 void pattern12(int n){
+ int space = 2*(n-1);
+for (int i =1; i<=n; i++){
+    for( int j = 1; j<=i; j++) cout << j;
+    for (int j=1; j<=space;j++) cout << " ";
+    for (int j =i; j>=1; j--) cout << j;
+    cout << endl;
+    space -= 2;
+}
+
+
 
 }
 
