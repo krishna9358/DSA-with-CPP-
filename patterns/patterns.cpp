74a1250f33d766d97caa6a477ccdfde876dcac77
Krishna Mohan <krishanmohank974@gmail.com>
2024-02-17 15:05:50 +0530

    Added pattern21

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -270,7 +270,7 @@ void pattern20(int n){
         }
         cout<<endl; 
     }
-            int inis = 8;
+            int inis = 2*n-2;
     for ( int i = 1; i<=n; i++){
 
         for (int j = 0; j < i; j++)
@@ -292,6 +292,25 @@ void pattern20(int n){
 
 }
 void pattern21(int n){
+    int space = 2*n-2;
+    for (int i = 1; i <=2*n-1; i++)
+    {
+        int stars = i;
+        if (i > n) stars = 2*n-i;
+        for (int j = 1; j<=stars;j++){
+            cout << "*" ;
+        }
+        for (int j = 1; j<= space; j++){
+            cout << " " ;
+        }
+        for (int j = 1; j<=stars;j++){
+            cout << "*" ;
+        }
+        cout <<endl;
+        if (i< n ) space -= 2;
+        else space += 2;
+    }
+    
 
 }
 void pattern22(int n){
